799556159319c935ea1ba95c3317326cb480bf61
Victor Gorgal <victorgorgal@gmail.com>
2025-06-16 13:33:00 -0300

    feat:

--- M210/Teorica/Projeto2/main.py
+++ M210/Teorica/Projeto2/main.py
@@ -5,83 +5,92 @@ class SimplexSolver:
         A: Coeficientes das restrições (<=).
         b: Termos independentes das restrições.
         """
-        self.original_c = c
-        self.A = [row + [0] * i + [1] + [0] * (len(b) - i - 1) for i, row in enumerate(A)]
-        self.b = b
-        self.num_vars = len(c)
-        self.num_constraints = len(b)
-        self.tableau = self._build_tableau(c)
-        self.basic_vars = [self.num_vars + i for i in range(self.num_constraints)]
+        self.funcao_objetivo = c
+        self.restricoes = [linha + [0] * i + [1] + [0] * (len(b) - i - 1) for i, linha in enumerate(A)]
+        self.termos_independentes = b
+        self.num_variaveis = len(c)
+        self.num_restricoes = len(b)
+        self.tabela = self._construir_tabela(c)
+        self.variaveis_basicas = [self.num_variaveis + i for i in range(self.num_restricoes)]
 
-    def _build_tableau(self, c):
-        tableau = [row + [bi] for row, bi in zip(self.A, self.b)]
-        last_row = [-ci for ci in c] + [0] * self.num_constraints + [0]
-        tableau.append(last_row)
-        return tableau
+    def _construir_tabela(self, c):
+        tabela = [linha + [bi] for linha, bi in zip(self.restricoes, self.termos_independentes)]
+        linha_objetivo = [-ci for ci in c] + [0] * self.num_restricoes + [0]
+        tabela.append(linha_objetivo)
+        return tabela
 
-    def _pivot(self, row: int, col: int):
-        pivot_element = self.tableau[row][col]
-        self.tableau[row] = [v / pivot_element for v in self.tableau[row]]
-        for r in range(len(self.tableau)):
-            if r != row:
-                ratio = self.tableau[r][col]
-                self.tableau[r] = [
-                    self.tableau[r][i] - ratio * self.tableau[row][i]
-                    for i in range(len(self.tableau[0]))
-                ]
+    def _pivoteamento(self, linha: int, coluna: int):
+        elemento_pivo = self.tabela[linha][coluna]
+        self.tabela[linha] = [v / elemento_pivo for v in self.tabela[linha]]
 
-    def solve(self):
+        for r in range(len(self.tabela)):
+            if r == linha:
+                continue
+
+            fator = self.tabela[r][coluna]
+            self.tabela[r] = [
+                self.tabela[r][i] - fator * self.tabela[linha][i]
+                for i in range(len(self.tabela[0]))
+            ]
+
+    def resolver(self):
         while True:
-            last_row = self.tableau[-1][:-1]
-            if all(c >= 0 for c in last_row):
+            ultima_linha = self.tabela[-1][:-1]
+
+            if all(c >= 0 for c in ultima_linha):
                 break
-            col = last_row.index(min(last_row))
-            ratios = []
-            for i in range(self.num_constraints):
-                if self.tableau[i][col] > 0:
-                    ratios.append(self.tableau[i][-1] / self.tableau[i][col])
+
+            coluna_pivo = ultima_linha.index(min(ultima_linha))
+            razoes = []
+            for i in range(self.num_restricoes):
+                if self.tabela[i][coluna_pivo] > 0:
+                    razoes.append(self.tabela[i][-1] / self.tabela[i][coluna_pivo])
                 else:
-                    ratios.append(float('inf'))
-            if all(r == float('inf') for r in ratios):
+                    razoes.append(float('inf'))
+
+            if all(r == float('inf') for r in razoes):
                 raise Exception("Solução ilimitada.")
-            row = ratios.index(min(ratios))
-            self.basic_vars[row] = col
-            self._pivot(row, col)
 
-    def get_solution(self):
-        solution = [0.0] * (self.num_vars + self.num_constraints)
-        for i, var in enumerate(self.basic_vars):
-            solution[var] = self.tableau[i][-1]
-        return solution[:self.num_vars], self.tableau[-1][-1]
+            linha_pivo = razoes.index(min(razoes))
+            self.variaveis_basicas[linha_pivo] = coluna_pivo
+            self._pivoteamento(linha_pivo, coluna_pivo)
 
-    def get_shadow_prices(self):
-        return self.tableau[-1][self.num_vars:self.num_vars + self.num_constraints]
+    def get_solucao(self):
+        solucao = [0.0] * (self.num_variaveis + self.num_restricoes)
 
-    def analyze_variation(self, delta_b):
-        new_rhs = []
-        for i in range(self.num_constraints):
+        for i, var in enumerate(self.variaveis_basicas):
+            solucao[var] = self.tabela[i][-1]
+
+        return solucao[:self.num_variaveis], self.tabela[-1][-1]
+
+    def get_precos_sombra(self):
+        return self.tabela[-1][self.num_variaveis:self.num_variaveis + self.num_restricoes]
+
+    def analisar_variacao(self, delta_b):
+        novos_rhs = []
+        for i in range(self.num_restricoes):
             temp = 0
-            for j in range(self.num_constraints):
-                temp += self.tableau[i][j+len(self.original_c)] * delta_b[j]
+            for j in range(self.num_restricoes):
+                temp += self.tabela[i][j + len(self.funcao_objetivo)] * delta_b[j]
+            temp += self.tabela[i][-1]
+            novos_rhs.append(temp)
 
-            temp += self.tableau[i][-1]
-            new_rhs.append(temp)
+        if all(novos_rhs[i] >= 0 for i in range(len(novos_rhs))):
+            precos_sombra = self.get_precos_sombra()
+            delta_z = sum(precos_sombra[i] * delta_b[i] for i in range(self.num_restricoes))
+            novo_z = self.tabela[-1][-1] + delta_z
+            return True, novo_z, precos_sombra
 
-        if all(new_rhs[i] >= 0 for i in range(len(new_rhs))):
-            shadow_prices = self.get_shadow_prices()
-            delta_z = sum(shadow_prices[i] * delta_b[i] for i in range(self.num_constraints))
-            new_z = self.tableau[-1][-1] + delta_z
-            return True, new_z, shadow_prices
-        else:
-            return False, self.tableau[-1][-1], []
+        return False, 0, []
 
 
 if __name__ == "__main__":
     # Exemplo:
-    # Max z = 3x + 5y
+    # Max z = 80x1 + 70x2 + 100x3 + 16x4
     # s.a.
-    #   x +  y <= 4
-    #   2x + 3y <= 9
+    #    x1 +  x2 +  x3 + 4x4 <= 250
+    #          x2 +  x3 + 2x4 <= 600
+    #   3x1 + 2x2 + 4x3       <= 500
 
     c = [80, 70, 100, 16]
     A = [
@@ -92,16 +101,18 @@ if __name__ == "__main__":
     b = [250, 600, 500]
 
     solver = SimplexSolver(c, A, b)
-    solver.solve()
+    solver.resolver()
 
-    sol, z = solver.get_solution()
+    sol, z = solver.get_solucao()
     print("Solução ótima:", sol)
     print("Valor ótimo:", z)
-    print("Preços sombra:", solver.get_shadow_prices())
+    print("Preços sombra:", solver.get_precos_sombra())
 
-    # Variação desejada em b
-    delta_b = [250, 0, 0]
-    viavel, novo_z, shadow_prices = solver.analyze_variation(delta_b)
+    # dx5 = -25
+    # dx6 = -60
+    # dx7 = -50
+    delta_b = [-25, -60, -50]
+    viavel, novo_z, shadow_prices = solver.analisar_variacao(delta_b)
     print("Alterações viáveis?", viavel)
     if viavel:
         print("Novo lucro ótimo:", novo_z)
